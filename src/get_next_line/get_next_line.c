383255eb0d7dc3b7da3ed7ed5a79ea5ef583ad21
FinestRyuk <40182073+ajilidali@users.noreply.github.com>
2024-02-15 02:53:59 +0100

    Update get_next_line.c

--- src/get_next_line/get_next_line.c
+++ src/get_next_line/get_next_line.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../include/get_next_line.h"
+#include "../../include/libft.h"
 
 static int	is_backslash(char *buffer)
 {
